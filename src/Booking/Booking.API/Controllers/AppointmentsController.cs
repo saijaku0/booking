e72231c79d8b3ca7b634667491c46bb58d4cd1cf
saijaku0 <minerking112256@gmail.com>
2025-12-28 17:40:55 +0100

    feat: add controller

---
+++ src/Booking/Booking.API/Controllers/AppointmentsController.cs
@@ -0,0 +1,31 @@
+﻿using Booking.Application.Appointments.Commands.CreateAppointment;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booking.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class AppointmentsController(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateAppointmentCommand command)
+        {
+            var id = await _mediator.Send(command);
+
+            return CreatedAtAction(
+                   nameof(GetById),
+                   new { id },
+                   id
+               );
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            return Ok(); 
+        }
+    }
+}
