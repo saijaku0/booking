1ec71e3dc22de483cd88eb8432ac226cc4f60aba
saijaku0 <minerking112256@gmail.com>
2026-01-02 16:30:20 +0100

    feat: add protection to appoinment

--- src/Booking/Booking.Application/Appointments/Commands/CreateAppointment/CreateAppointmentCommandHandler.cs
+++ src/Booking/Booking.Application/Appointments/Commands/CreateAppointment/CreateAppointmentCommandHandler.cs
@@ -1,6 +1,8 @@
 ﻿using Booking.Application.Common.Interfaces;
 using Booking.Domain.Entities;
+using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Booking.Application.Appointments.Commands.CreateAppointment
 {
@@ -10,6 +12,21 @@ namespace Booking.Application.Appointments.Commands.CreateAppointment
 
         public async Task<Guid> Handle(CreateAppointmentCommand request, CancellationToken cancellationToken)
         {
+            var isOverLap = await _context.Appointments
+                .AnyAsync(x =>
+                    x.ResourceId == request.ResourceId
+                        &&
+                    request.StartTime < x.EndTime
+                        &&
+                    request.EndTime > x.StartTime,
+                    cancellationToken);
+
+            if (isOverLap)
+            {
+                var failure = new FluentValidation.Results.ValidationFailure("TimeSlot", "This time slot is already taken");
+                throw new ValidationException([failure]);
+            }
+
             Appointment appointment = new(
                 request.CustomerId,
                 request.ResourceId,
