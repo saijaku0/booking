3a7664330b4f97f47969f3c65207e284ecb39bf3
saijaku0 <minerking112256@gmail.com>
2026-01-03 14:23:52 +0100

    feat: add registration for user

--- src/Booking/Booking.API/Program.cs
+++ src/Booking/Booking.API/Program.cs
@@ -2,8 +2,10 @@ using Booking.API.Infrastructure;
 using Booking.Application.Appointments.Commands.CreateAppointment;
 using Booking.Application.Common.Behaviors;
 using Booking.Application.Common.Interfaces;
+using Booking.Domain.Entities;
 using Booking.Infrastructure.Persistence;
 using FluentValidation;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
@@ -23,6 +25,15 @@ builder.Services.AddProblemDetails();
 
 builder.Services.AddDbContext<BookingDbContext>(options => 
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+    {
+        options.Password.RequireDigit = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequiredLength = 6;
+    })
+    .AddEntityFrameworkStores<BookingDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddScoped<IBookingDbContext>(provider =>
     (IBookingDbContext)provider.GetRequiredService<BookingDbContext>());
