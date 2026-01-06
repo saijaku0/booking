879ec040a454608c1d13ab2a7480d3a7257d7fd2
saijaku0 <minerking112256@gmail.com>
2026-01-06 17:12:53 +0100

    feat: add doctor

---
+++ src/Booking/Booking.Domain/Entities/Doctor.cs
@@ -0,0 +1,9 @@
+﻿namespace Booking.Domain.Entities
+{
+    public class Doctor
+    {
+        public Guid Id { get; init; }
+        public string Name { get; set; } = string.Empty;
+        public string Specialty { get; set; } = string.Empty;
+    }
+}
