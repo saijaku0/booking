255e0e45679a08fba6f84aaa5501e991847c9de9
saijaku0 <minerking112256@gmail.com>
2026-01-07 13:20:34 +0100

    feat: add validator

---
+++ src/Booking/Booking.Application/Admin/Commands/CreateDoctor/CreateDoctorCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Booking.Application.Admin.Commands.CreateDoctor
+{
+    internal class CreateDoctorCommandValidator : AbstractValidator<CreateDoctorCommand>
+    {
+        public CreateDoctorCommandValidator() 
+        {
+            RuleFor(v => v.Email).NotEmpty().EmailAddress();
+            RuleFor(v => v.Password).NotEmpty().MinimumLength(6);
+            RuleFor(v => v.Name).NotEmpty();
+        }
+    }
+}
