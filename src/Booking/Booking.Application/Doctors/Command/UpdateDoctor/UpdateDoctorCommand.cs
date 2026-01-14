f086c32442647e84b08a271c53a466aa4c3c9929
saijaku0 <minerking112256@gmail.com>
2026-01-14 14:13:47 +0100

    feat: add update doctor command

---
+++ src/Booking/Booking.Application/Doctors/Command/UpdateDoctor/UpdateDoctorCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Booking.Application.Doctors.Command.UpdateDoctor
+{
+    public record UpdateDoctorCommand(
+        string Name,
+        string Lastname,
+        string Specialty,
+        string Bio,
+        int ExperienceYears,
+        string ImageUrl,
+        decimal ConsultationFee,
+        bool IsActive) : IRequest<Unit>;
+}
