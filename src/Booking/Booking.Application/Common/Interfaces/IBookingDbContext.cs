9ca9a71cc1511be54793bc659a4970700f311c17
saijaku0 <minerking112256@gmail.com>
2026-01-14 14:22:33 +0100

    refactor: update interface make import simple

--- src/Booking/Booking.Application/Common/Interfaces/IBookingDbContext.cs
+++ src/Booking/Booking.Application/Common/Interfaces/IBookingDbContext.cs
@@ -1,14 +1,12 @@
 ﻿using Booking.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
-using DoctorEntity = Booking.Domain.Entities.Doctor;
-
 namespace Booking.Application.Common.Interfaces
 {
     public interface IBookingDbContext
     {
         DbSet<Appointment> Appointments { get; }
-        DbSet<DoctorEntity> Doctors { get; }
+        DbSet<Doctor> Doctors { get; }
         Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
