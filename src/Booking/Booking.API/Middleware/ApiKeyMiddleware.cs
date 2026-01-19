002a3f1f711c1c7e047638b4baad0aa36e9d4e87
saijaku0 <minerking112256@gmail.com>
2026-01-19 11:46:43 +0100

    feat: add middleware for api key

---
+++ src/Booking/Booking.API/Middleware/ApiKeyMiddleware.cs
@@ -0,0 +1,31 @@
+﻿namespace Booking.API.Middleware;
+
+public class ApiKeyMiddleware(RequestDelegate next, IConfiguration configuration)
+{
+    private readonly RequestDelegate _next = next;
+    private readonly IConfiguration _configuration = configuration;
+
+    private const string APIKEYNAME = "X-API-KEY";
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (!context.Request.Headers.TryGetValue(APIKEYNAME, out var extractedApiKey))
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("API Key was not provided.");
+            return;
+        }
+
+        var appSettings = _configuration.GetSection("ApiSettings");
+        var apiKey = appSettings.GetValue<string>("SecretKey");
+
+        if (!apiKey.Equals(extractedApiKey))
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("Unauthorized client.");
+            return;
+        }
+
+        await _next(context);
+    }
+}
