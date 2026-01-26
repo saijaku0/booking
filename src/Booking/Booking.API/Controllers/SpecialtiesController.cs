67b73d610a78d85940e6d7fb706f81eeccd6f97a
saijaku0 <minerking112256@gmail.com>
2026-01-26 11:21:24 +0100

    feat: add get specialty method by id

---
+++ src/Booking/Booking.API/Controllers/SpecialtiesController.cs
@@ -0,0 +1,22 @@
+﻿using Booking.Application.Specialties.Dtos;
+using Booking.Application.Specialties.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booking.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class SpecialtiesController(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+
+        [HttpGet]
+        [AllowAnonymous] 
+        public async Task<ActionResult<List<SpecialtyDto>>> GetList()
+        {
+            return await _mediator.Send(new GetSpecialtiesQuery());
+        }
+    }
+}
---
+++ src/Booking/Booking.Application/Specialties/Queries/GetSpecialtyByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using Booking.Application.Common.Exceptions;
+using Booking.Application.Common.Interfaces;
+using Booking.Application.Specialties.Dtos;
+using Booking.Domain.Entities;
+using MediatR;
+
+namespace Booking.Application.Specialties.Queries
+{
+    public class GetSpecialtyByIdQueryHandler(IBookingDbContext context)
+    : IRequestHandler<GetSpecialtyByIdQuery, SpecialtyDto>
+    {
+        private readonly IBookingDbContext _context = context;
+
+        public async Task<SpecialtyDto> Handle(
+            GetSpecialtyByIdQuery request,
+            CancellationToken cancellationToken)
+        {
+            var entity = await _context.Specialties
+                .FindAsync([request.Id], cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(Specialty), request.Id);
+            }
+
+            return new SpecialtyDto
+            {
+                Id = entity.SpecialtyId,
+                Name = entity.Name
+            };
+        }
+    }
+}
