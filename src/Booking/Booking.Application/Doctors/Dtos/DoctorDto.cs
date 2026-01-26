a89c02c61561903efe907b36bc01def100d09264
saijaku0 <minerking112256@gmail.com>
2026-01-26 10:43:36 +0100

    fix: update specialty for doctor

--- src/Booking/Booking.Application/Doctors/Dtos/DoctorDto.cs
+++ src/Booking/Booking.Application/Doctors/Dtos/DoctorDto.cs
@@ -5,6 +5,6 @@
         public Guid Id { get; init; }
         public string Name { get; set; } = string.Empty;
         public string Lastname { get; set; } = string.Empty;
-        public string Specialty { get; set; } = string.Empty;
+        public Guid SpecialtyId { get; set; }
     }
 }
