b716e00b30245faa269b92a0fac85fbf7f3f855a
saijaku0 <minerking112256@gmail.com>
2026-01-27 11:25:39 +0100

    feat: add controller for reviews

---
+++ src/Booking/Booking.API/Controllers/ReviewsController.cs
@@ -0,0 +1,22 @@
+﻿using Booking.Application.Reviews.Commands.CreateReview;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Booking.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReviewsController(IMediator mediatR) : ControllerBase
+    {
+        private readonly IMediator _mediatR = mediatR;
+
+        [HttpPost]
+        public async Task<IActionResult> CreateReview(CreateReviewCommand request)
+        {
+            await _mediatR.Send(request);
+            return Ok();
+        }
+    }
+}
