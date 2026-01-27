c49134db93a0482d5ff548f5ef0bb65f6c59acbf
saijaku0 <minerking112256@gmail.com>
2026-01-27 11:25:21 +0100

    feat: add create review command

---
+++ src/Booking/Booking.Application/Reviews/Commands/CreateReview/CreateReviewCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Booking.Application.Reviews.Commands.CreateReview
+{
+    public record CreateReviewCommand(
+        Guid DoctorId, 
+        int Rating)
+        : IRequest<Guid>; 
+}
---
+++ src/Booking/Booking.Application/Reviews/Commands/CreateReview/CreateReviewCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Booking.Application.Common.Interfaces;
+using Booking.Domain.Entities;
+using MediatR;
+
+namespace Booking.Application.Reviews.Commands.CreateReview
+{
+    public class CreateReviewCommandHandler(IBookingDbContext context) 
+        : IRequestHandler<CreateReviewCommand, Guid>
+    {
+        private readonly IBookingDbContext _context = context;
+        public async Task<Guid> Handle(
+            CreateReviewCommand request, 
+            CancellationToken cancellationToken)
+        {
+            var doctor = await _context.Doctors
+                .FindAsync(
+                    [ request.DoctorId ],
+                    cancellationToken);
+
+            if (doctor == null) 
+                throw new ArgumentException("DoctorId cannot be empty.");
+
+            var review = new Review
+            (
+                request.DoctorId,
+                request.Rating
+            );
+            _context.Reviews.Add(review);
+            doctor.AddReview(request.Rating);
+            await _context.SaveChangesAsync(cancellationToken);
+            return review.ReviewId;
+        }
+    }
+}
