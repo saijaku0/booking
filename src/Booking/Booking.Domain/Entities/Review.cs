a047895c36130c03e540115d021b993e1b764a67
saijaku0 <minerking112256@gmail.com>
2026-01-27 11:03:07 +0100

    feat: add review domain

---
+++ src/Booking/Booking.Domain/Entities/Review.cs
@@ -0,0 +1,27 @@
+﻿namespace Booking.Domain.Entities
+{
+    public class Review
+    {
+        public Guid ReviewId { get; private set; }
+        public Guid DoctorId { get; private set; }
+        public Doctor Doctor { get; private set; } = null!;
+        public int Rating { get; private set; }
+
+        private Review() { }
+
+        public Review(Guid doctorId, int rating)
+        {
+            ReviewId = Guid.NewGuid();
+            DoctorId = doctorId;
+            Rating = rating;
+        }
+
+        public void UpdateRating(int newRating)
+        {
+            if (newRating < 1 || newRating > 5)
+                throw new ArgumentOutOfRangeException(nameof(newRating), "Rating must be between 1 and 5.");
+
+            Rating = newRating;
+        }
+    }
+}
