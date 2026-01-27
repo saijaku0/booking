80d53240e7b3204a9898042d988d091384ee4ad2
saijaku0 <minerking112256@gmail.com>
2026-01-27 11:51:22 +0100

    feat: add dto and availability query

---
+++ src/Booking/Booking.Application/Appointments/Dtos/TimeSlotDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Booking.Application.Appointments.Dtos
+{
+    public class TimeSlotDto
+    {
+        public DateTime Start { get; init; }
+        public DateTime End { get; init; }
+    }
+}
---
+++ src/Booking/Booking.Application/Appointments/Queries/GetDoctorAvailability/GetDoctorAvailabilityQuery.cs
@@ -0,0 +1,10 @@
+﻿using Booking.Application.Appointments.Dtos;
+using MediatR;
+
+namespace Booking.Application.Appointments.Queries.GetDoctorAvailability
+{
+    public record GetDoctorAvailabilityQuery(
+        Guid DoctorId, 
+        DateTime Date)
+        : IRequest<List<TimeSlotDto>>;
+}
