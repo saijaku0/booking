f0091e48c306777adb113341fdf41cd560c98ff1
saijaku0 <minerking112256@gmail.com>
2026-02-03 13:32:00 +0100

    feat: add interface to storeg file

---
+++ src/Booking/Booking.Application/Common/Interfaces/IFileStorageService.cs
@@ -0,0 +1,7 @@
+﻿namespace Booking.Application.Common.Interfaces
+{
+    public interface IFileStorageService
+    {
+        public Task<string> UploadFileAsync(Stream fileStream, string fileName, string contentType);
+    }
+}
