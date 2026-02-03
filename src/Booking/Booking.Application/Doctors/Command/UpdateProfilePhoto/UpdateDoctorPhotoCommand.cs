1251e6aabf6628fe259e3bebd380eac7722cbeb5
saijaku0 <minerking112256@gmail.com>
2026-02-03 13:42:00 +0100

    feat: add update doctor photo command

---
+++ src/Booking/Booking.Application/Doctors/Command/UpdateProfilePhoto/UpdateDoctorPhotoCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Booking.Application.Doctors.Command.UpdateProfilePhoto
+{
+    public record UpdateDoctorPhotoCommand(
+        Stream PhotoStream,
+        string FileName,
+        string ContentType) : IRequest<Unit>;
+}
