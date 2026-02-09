74ecc05ad462ed24c49963220eb0501cf64d890d
saijaku0 <minerking112256@gmail.com>
2026-02-09 11:46:35 +0100

    feat: add infrastructure service injection

---
+++ src/Booking/Booking.Infrastructure/Services/IIdentityService.cs
@@ -0,0 +1,40 @@
+﻿using Booking.Application.Common.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace Booking.Infrastructure.Services
+{
+
+    public class IdentityService(
+        UserManager<IdentityUser> userManager,
+        IUserClaimsPrincipalFactory<IdentityUser> userClaimsPrincipalFactory,
+        IAuthorizationService authorizationService) : IIdentityService
+    {
+        private readonly UserManager<IdentityUser> _userManager = userManager;
+        private readonly IUserClaimsPrincipalFactory<IdentityUser> _userClaimsPrincipalFactory = userClaimsPrincipalFactory;
+        private readonly IAuthorizationService _authorizationService = authorizationService;
+
+        public async Task<string?> GetUserNameAsync(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            return user?.UserName;
+        }
+
+        public async Task<bool> IsInRoleAsync(string userId, string role)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            return user != null && await _userManager.IsInRoleAsync(user, role);
+        }
+
+        public async Task<bool> AuthorizeAsync(string userId, string policyName)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) return false;
+
+            var principal = await _userClaimsPrincipalFactory.CreateAsync(user);
+            var result = await _authorizationService.AuthorizeAsync(principal, policyName);
+
+            return result.Succeeded;
+        }
+    }
+}
