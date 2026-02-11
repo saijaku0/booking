03124cd3428f9a9dacc388c00bb6c0e512c5044b
saijaku0 <minerking112256@gmail.com>
2026-02-11 13:31:59 +0100

    feat: add confirm appointment method

---
+++ src/Booking/Booking.Application/Appointments/Commands/ConfirmAppointment/ConfirmAppointmentCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace Booking.Application.Appointments.Commands.ConfirmAppointment
+{
+    public record ConfirmAppointmentCommand(Guid AppointmentId) 
+        : IRequest<Unit>;
+}
