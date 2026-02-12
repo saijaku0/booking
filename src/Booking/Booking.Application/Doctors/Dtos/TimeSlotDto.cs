355160556c750aad38475e7e72a8995613f34c43
saijaku0 <minerking112256@gmail.com>
2026-02-12 11:36:48 +0100

    feat: add time slot dto

---
+++ src/Booking/Booking.Application/Doctors/Dtos/TimeSlotDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Booking.Application.Doctors.Dtos
+{
+    public class TimeSlotDto
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
