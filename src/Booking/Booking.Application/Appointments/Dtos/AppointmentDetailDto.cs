a04396f157e19970a2cb7224ddb9c23d1fe4995e
saijaku0 <minerking112256@gmail.com>
2026-02-15 16:46:23 +0100

    refactor: move dublicates

--- src/Booking/Booking.Application/Appointments/Dtos/AppointmentDetailDto.cs
+++ src/Booking/Booking.Application/Appointments/Dtos/AppointmentDetailDto.cs
@@ -1,24 +1,9 @@
 ﻿
 namespace Booking.Application.Appointments.Dtos
 {
-    public record AppointmentDetailDto
+    public record AppointmentDetailDto : AppointmentBaseDto
     {
-        public Guid Id { get; init; }
-        public Guid DoctorId { get; init; }
-        public Guid PatientId { get; init; }
-
-        public string PatientName { get; set; } = string.Empty;
-        public string DoctorName { get; init; } = string.Empty;
-        public string Specialty { get; init; } = string.Empty;
-
-        public string? DoctorPhotoUrl { get; init; }
-        public string? DoctorPhoneNumber { get; init; }
-        public decimal Price { get; init; }
-
-        public string Status { get; init; } = string.Empty;
-        public string? MedicalNotes {  get; init; } = string.Empty;
         public List<AttachmentDto> Attachments { get; init; } = [];
-        public DateTime StartTime { get; init; }
-        public DateTime EndTime { get; init; }
+
     }
 }
