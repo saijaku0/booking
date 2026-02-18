8d0f1898246943edb2017af1e6ac1860342817e7
saijaku0 <minerking112256@gmail.com>
2026-02-18 22:02:04 +0100

    feat: add xml documentation

--- src/Booking/Booking.Application/Common/Extension/AppointmentQueryExtensions.cs
+++ src/Booking/Booking.Application/Common/Extension/AppointmentQueryExtensions.cs
@@ -4,6 +4,13 @@ namespace Booking.Application.Common.Extension
 {
     public static class AppointmentQueryExtensions
     {
+        /// <summary>
+        /// Filters appointments that overlap with the specified time interval.
+        /// </summary>
+        /// <param name="doctorId">Optional doctor ID to filter by. If null, all doctors are considered.</param>
+        /// <param name="start">Start of the interval (exclusive on the right).</param>
+        /// <param name="end">End of the interval (exclusive on the left).</param>
+        /// <returns>An IQueryable of appointments that overlap with the interval.</returns>
         public static IQueryable<Appointment> WhereOverlaps(
         this IQueryable<Appointment> query,
         Guid? doctorId,
